78054ea41c4723b56e5862950c85159f9eb5a012
Josue Monteiro <josue.monteiro@innotech.pt>
2024-09-03 19:19:09 +0100

    Implement concurrent processing for notifications using goroutines to enhance performance and responsiveness

--- notifications-service/internal/services/notifications.go
+++ notifications-service/internal/services/notifications.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"notifications-service/internal/utils"
 	"notifications-service/models"
+	"sync"
 	"time"
 )
 
@@ -37,25 +38,37 @@ func notificationService(RedisService *RedisService, vehiclesHashMap *map[string
 		return
 	}
 
+	// Create Go routines to process each notification concurrently
+	var wg sync.WaitGroup
+	errChan := make(chan error, len(notifications))
+
 	for _, notification := range notifications {
-		vehicle, ok := (*vehiclesHashMap)[notification.PatternId]
-		if !ok {
-			// fmt.Printf("Vehicle with PatternId %s not found\n", notification.PatternId)
-			continue
-		}
+		wg.Add(1)
 
-		// Define a point and a polygon
-		point := models.Point{X: vehicle.Lon, Y: vehicle.Lat}
-		polygon := []models.Point{}
-		for _, coordinate := range notification.GeoJSON.Geometry.Coordinates[0] {
-			polygon = append(polygon, models.Point{X: coordinate[0], Y: coordinate[1]})
-		}
+		go func(notification models.Notification) {
+			defer wg.Done()                    // Decrement the counter when the goroutine completes
+			vehicle, ok := (*vehiclesHashMap)[notification.PatternId]
+			if !ok {
+				// fmt.Printf("Vehicle with PatternId %s not found\n", notification.PatternId)
+				return
+			}
 
-		//Check if the bus is in the radius of stop
-		if utils.PointInPolygon(point, polygon) {
-			fmt.Printf("Bus %s is in a radius of %v %s Stop %s\n", vehicle.Id, notification.Distance, notification.DistanceUnit ,notification.StopId)
-		}
+			// Define a point and a polygon
+			point := models.Point{X: vehicle.Lon, Y: vehicle.Lat}
+			polygon := []models.Point{}
+			for _, coordinate := range notification.GeoJSON.Geometry.Coordinates[0] {
+				polygon = append(polygon, models.Point{X: coordinate[0], Y: coordinate[1]})
+			}
+
+			//Check if the bus is in the radius of stop
+			if utils.PointInPolygon(point, polygon) {
+				fmt.Printf("Bus %s is in a radius of %v %s Stop %s\n", vehicle.Id, notification.Distance, notification.DistanceUnit ,notification.StopId)
+			}
+		}(notification)
 	}
+
+	wg.Wait()      // Wait for all goroutines to complete
+	close(errChan) // Close the error channel as no more errors will be sent
 }
 
 func getNotifications(redisService *RedisService) ([]models.Notification, error) {
